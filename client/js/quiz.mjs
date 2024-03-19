e90f314ebe4c803f4116c6c22411af1de2bdfc64
Bebop2000 <schnumma@gmail.com>
2024-03-19 11:14:31 +0000

    Added psql database access

--- client/js/quiz.mjs
+++ client/js/quiz.mjs
@@ -4,48 +4,50 @@
 
 //let div = document.createElement("div");
 
+
+
 let testJson = '{"questions": [' +
     '{ "question": "What is 1+1"},' +
     '{ "question": "What is 3*3"},' +
     '{ "question": "What is 2+2"}' +
     ']}'
 
-const obj = JSON.parse(testJson);
+// const obj = JSON.parse(testJson);
 
-class Question {
-    id;
-    questionText;
-    answerText;
-    constructor(id, qText) {
-        this.id = id;
-        this.questionText = qText;
-    }
-};
+// class Question {
+//     id;
+//     questionText;
+//     answerText;
+//     constructor(id, qText) {
+//         this.id = id;
+//         this.questionText = qText;
+//     }
+// };
 
-let div = document.createElement("div");
-// div.setAttribute("class", "mb-3");
-let form = document.createElement("form");
-let questions = [];
+// let div = document.createElement("div");
+// // div.setAttribute("class", "mb-3");
+// let form = document.createElement("form");
+// let questions = [];
 
-for (let i = 0; i < obj.questions.length; i++) {
-    let q = obj.questions[i];
-    //questions.push(Question("q" + i, q.question, q.answer));
+// for (let i = 0; i < obj.questions.length; i++) {
+//     let q = obj.questions[i];
+//     //questions.push(Question("q" + i, q.question, q.answer));
 
-    let questionText = document.createTextNode(q.question);
-    questionText.htmlFor = "q" + i;
-    form.append(questionText);
+//     let questionText = document.createTextNode(q.question);
+//     questionText.htmlFor = "q" + i;
+//     form.append(questionText);
     
-    let q1 = document.createElement("input");
-    q1.type = "text";
-    q1.id = "q" + i;
-    form.append(q1);
-    form.append(document.createElement("br"));
-}
+//     let q1 = document.createElement("input");
+//     q1.type = "text";
+//     q1.id = "q" + i;
+//     form.append(q1);
+//     form.append(document.createElement("br"));
+// }
 
-div.append(form);
-
-
+// div.append(form);
 
 
 
-document.body.append(form);
+
+
+// document.body.append(form);
--- server.mjs
+++ server.mjs
@@ -1,7 +1,33 @@
+// lets me use require instead of import which is needed for pg-promise for some reason
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+
 import express from 'express';
+const pgp = require('pg-promise')();
+
+
+const cn = {
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: '',
+    database: 'test'
+}
+
+const db = pgp(cn);
+
 
 const server = express();
 
-server.use(express.static("client"));
+
+//server.use(express.static("client"));
+server.get('/', (req, res) => {
+    let txt = "";
+    db.any('INSERT INTO test_table (id, info) VALUES($1, $2)', [5, 'ARRRG']);
+    res.send(txt);
+  })
+
+
 
 server.listen(8080);
