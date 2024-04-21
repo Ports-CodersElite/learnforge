31146183d0aa93cadf1404597617c42ada8a3d88
N1ckell <nbknight15@gmail.com>
2024-04-21 10:29:27 +0100

    added dashboard css

---
+++ client/js/navbar.js
@@ -0,0 +1,19 @@
+
+let nav_toggle = document.querySelector("#nav-btn");
+let nav_ = document.querySelector("#nav");
+
+nav_toggle.addEventListener("click", navBarToggle)
+
+function navBarToggle(){
+    console.log("test")
+    nav_.classList.add("nav-transition");
+    console.log(nav_.classList)
+}
+
+/*function navBarToggle(){
+    if(nav_.classList.contains('nav-transition')){
+        nav_.classList.remove('nav-transition')
+    } else {
+        nav_.classList.add('nav-transition')
+    }
+}*/
