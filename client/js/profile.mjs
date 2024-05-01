e6ae3599aa6498fff4e37ccdb62e210c72b394a4
up2108272 <up2108272@myport.ac.uk>
2024-05-01 17:53:55 +0100

    working on getting profile page functional
    Doesn't work yet

--- client/js/profile.mjs
+++ client/js/profile.mjs
@@ -1,1 +1,29 @@
 import * as auth from './auth.mjs';
+import * as dbq from './db_query.mjs';
+
+window.addEventListener("load", onLoad);
+
+let firstNameLabel = document.getElementById("firstNameLabel");
+let middleNameLabel = document.getElementById("middleNameLabel");
+let lastNameLabel = document.getElementById("lastNameLabel");
+let emailLabel = document.getElementById("emailLabel");
+let idLabel = document.getElementById("idLabel");
+
+let userData;
+
+function updateUserDetails() {
+    dbq.getProfileData(sessionStorage.getItem("uid"), (res) => {
+        userData = (res);
+        console.log(res[0]);
+        firstNameLabel.innerHTML = res[0];
+        middleNameLabel.innerHTML = res[1];
+        lastNameLabel.innerHTML = res[2];
+        emailLabel.innerHTML = res[3];
+        idLabel.innerHTML = sessionStorage.getItem("uid");
+    });
+}
+
+
+function onLoad() {
+    updateUserDetails();
+}
--- server/db_interface.mjs
+++ server/db_interface.mjs
@@ -3,12 +3,21 @@ import sqlite3 from 'sqlite3';
 let db;
 let dbOpened = false
 
-export function openDb() {
+export function openDb(callback) {
     if (!dbOpened) {
         db = new sqlite3.Database("./server/learnforge.db", sqlite3.OPEN_READWRITE, (err) =>{
-            if (err) return console.error(err.message);
-            else dbOpened = true;
+            if (err) {
+                return console.error(err.message);
+                callback(false);
+            }
+            else {
+                dbOpened = true;
+                callback(true);
+            }
         });
+    }
+    else {
+        callback(true);
     }
 }
 
@@ -21,10 +30,13 @@ export function createStudent(id, fname, mname, lname, email) {
 }
 
 export function createLecturer(id, fname, mname, lname, email) {
-    openDb()
-    let sql = `INSERT INTO lecturer_details VALUES (?, ?, ?, ?, ?)`
-    db.run(sql, [id, fname, mname, lname, email], (err) => {
-        if (err) return console.error(err.message);
+    openDb((res) =>{
+        if(res) {
+            let sql = `INSERT INTO lecturer_details VALUES (?, ?, ?, ?, ?)`
+            db.run(sql, [id, fname, mname, lname, email], (err) => {
+                if (err) return console.error(err.message);
+            })
+        }
     })
 }
 
@@ -51,11 +63,39 @@ export function displayTable(table) {
 }
 
 
-export async function getProfile(uid, callback) {
-    openDb();
+export function getProfile(uid, callback) {
+    openDb((res) => {
         let sql = "SELECT * FROM student_details WHERE student_id =" + "'" + uid + "'" + "UNION SELECT * FROM lecturer_details WHERE lecturer_id =" + "'" + uid + "'";
         db.get(sql, [], (err, row) => {
             if (err) return console.error(err.message);
-            callback([row.user_fname, row.user_mname, row.user_lname, row.user_emailaddress]);
-        });
+            if (row == undefined) {
+                callback(['ERROR']);
+            }
+            else {
+                callback([row.user_fname, row.user_mname, row.user_lname, row.user_emailaddress]);
+            }
+        });    
+    });
 }
+
+export function updateUserProfile(uid, column, newValue,  callback) {
+    let sql1 = `UPDATE student_details SET ` + column + ` = ` + newValue + ` WHERE 'student_id' = ` + uid + `;`;
+    let sql2 = `UPDATE lecturer_details SET ` + column + ` = ` + newValue + ` WHERE 'lecturer_id' = ` + uid + `;`;
+    openDb((res) => {
+        db.run(sql1, (err) => {
+            if (err) {
+                console.log(err.message);
+            }
+        })
+        db.run(sql2, (err) => {
+            if (err) {
+                console.log(err.message);
+            }
+        })
+    });
+
+
+}
+
+updateUserProfile("std1", "user_mname", "mname", (callback) => {});
+displayTable("student_details");
