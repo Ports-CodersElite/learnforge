e176061cdd999df627d4b1989c1b04c1ee85a28a
up2108272 <up2108272@myport.ac.uk>
2024-05-06 15:42:59 +0100

    Added more db functions and routes

--- client/testing/testdb.mjs
+++ client/testing/testdb.mjs
@@ -1,13 +1,24 @@
 import * as dbq from '../js/db_query.mjs';
 
 
-let uid = document.querySelector("#uidInput");
-let title = document.querySelector("#titleInput");
-let body = document.querySelector("#bodyInput");
+let id = document.querySelector("#idInput");
+//let className = document.querySelector("#classNameInput");
+let joinCode = document.querySelector("#joinCodeInput");
 let submit = document.querySelector("#submit");
 submit.addEventListener("click", submitUserToDatabase);
 
 function submitUserToDatabase() {
-    console.log(uid.value, title.value, body.value);
-    dbq.uploadQuiz(uid.value, title.value, body.value);
+    console.log(id.value, joinCode.value);
+    let payload = [id.value, joinCode.value];
+    fetch("/join-class", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    })
+    .then((res) => res.json())
+    .then((resData) =>{
+        console.log(resData);
+    })
 }
