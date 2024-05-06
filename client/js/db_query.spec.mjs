80c8629bdcddecb9393cbd87f3d2e3c0be5d8a48
Arnav <arnavsharma25@yahoo.com>
2024-05-06 18:43:58 +0100

    minr changes

--- client/js/db_query.spec.mjs
+++ client/js/db_query.spec.mjs
@@ -1,7 +1,7 @@
 import * as dbq from './db_query.mjs';
 // 9KjGwwlF5AYqHIo2OzoV1AGj84x2 (user id for testing)
 
-describe("Creating a dummy user", function(){
+describe("Testing database Upload Data", function(){
     it("Creating a dummy user", async function(){
         const user = await dbq.submitUser('45', 'firstname', 'middlename', 'lastname', 'test@email.com', 'student');
         console.log(user);
@@ -9,9 +9,3 @@ describe("Creating a dummy user", function(){
     });
 })
 
-describe("Get Profile Data", function(){
-    it("Getting Profile data from user (9KjGwwlF5AYqHIo2OzoV1AGj84x2)", async function(){
-        const user = await dbq.getProfileData('9KjGwwlF5AYqHIo2OzoV1AGj84x2');
-        expect(user).toEqual();
-    });
-})
--- client/js/signup.mjs
+++ client/js/signup.mjs
@@ -8,12 +8,24 @@ let buttons = {
     teacherDropdown: null
 }
 
+let inputs = {
+    email: null,
+    fname: null,
+    lname: null,
+    passwordInput: null,
+}
+
 let selectedRole = null;
 
 function eventListeners() {
     buttons.signupBtn = document.querySelector("#signupBtn");
     buttons.studentDropdown = document.querySelector("#studentDropdown");
     buttons.teacherDropdown = document.querySelector("#teacherDropdown");
+    inputs.fname = document.querySelector('#fnameInput');
+    inputs.lname = document.querySelector('#lnameInput');
+    inputs.email = document.querySelector('#emailInput');
+    inputs.passwordInput = document.querySelector('#passwordInput');
+
 
     buttons.signupBtn.addEventListener("click", Signup);
     buttons.studentDropdown.addEventListener("click", StudentDropdownFunc);
@@ -26,11 +38,10 @@ function Signup() {
         alert("Please Select a role");
         return;
     }
-    const email = String(emailInput.value);
-    const fname = String(fnameInput.value);
-
-    const lname = String(lnameInput.value);
-    const pw = String(passwordInput.value);
+    const email = String(inputs.email.value);
+    const fname = String(inputs.fname.value);
+    const lname = String(inputs.lname.value);
+    const pw = String(inputs.passwordInput.value);
     auth.createUser(fname, null, lname, email, pw, selectedRole, "../index.html");
 }
 
